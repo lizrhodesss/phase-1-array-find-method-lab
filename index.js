7deb75411de8228c2245c5f4d107704fbea7a644
liz <rhodes.liz@gmail.com>
2022-06-29 18:43:23 -0400

    commit tree 91c3d31b5fa7ae661538a4b9fa8fe478f382699a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+
+function superbowlWins(record){
+    return (record === "W")
+}
+Array.prototype.find(record)
+ 
